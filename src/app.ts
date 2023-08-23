a0a2290f9f60f5c4e0febd44ff6d01d48ac4458c
Shajeeb <liveclasswd@gmail.com>
2023-08-23 12:40:49 +0600

    testing lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
-const port = 5000;
 
 // cors use
 app.use(cors());
@@ -10,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("oh amar jan pakhi !");
+app.get('/', (req: Request, res: Response) => {
+  res.send('oh amar jan pakhi !');
 });
 
 export default app;
