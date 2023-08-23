dfac7c80a68e7d712b23dd017756f25a96fd082d
Shajeeb <liveclasswd@gmail.com>
2023-08-23 12:41:38 +0600

    testing lint-staged

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.databse_url as string);
-    console.log("Database connect successfully");
+    console.log('Database connect successfully');
 
     app.listen(config.port, () => {
       console.log(`Database listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log("Failed to connect the databse", error);
+    console.log('Failed to connect the databse', error);
   }
 }
 
