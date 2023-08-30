8c4aa01851773c635bad50aab2276196f7f72c82
Shajeeb <liveclasswd@gmail.com>
2023-08-30 15:17:43 +0600

    add zod validation

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,20 @@ import { userService } from './user.service';
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    /*  req validation 
+        body akta object  
+        data gulao object
+    */
+
+    // const createUserZodSchema = z.object({
+    //   body: z.string({
+    //     required_error: 'role is required',
+    //   }),
+    //   password: z.string().optional(),
+    // });
+
+    // await createUserZodSchema.parseAsync(req);
+
     const { user } = req.body;
     const result = await userService.createUser(user);
     res.status(200).json({
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import validateRequest from '../../middleWares/validateRequest';
 import { userController } from './user.controller';
+import { userValidation } from './user.validation';
 const router = express.Router();
 
-router.post('/create-user', userController.createUser);
+router.post(
+  '/create-user',
+  validateRequest(userValidation.createUserZodSchema),
+  userController.createUser,
+);
 
 export const userRoutes = router;
